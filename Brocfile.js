5479c2b4714d4a4178ae945406b10a1aa3a79965
Austin Trafficante <austin.trafficante@gmail.com>
2014-11-21 11:50:31 -0800

    Removed unncessary bower-recaptcha-ajax import from Brocfile.js

--- Brocfile.js
+++ Brocfile.js
@@ -4,9 +4,6 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp();
 
-app.import('bower_components/recaptcha-ajax/recaptcha_ajax.js');
-app.import('vendor/captcha/captcha.js');
-
 // Use `app.import` to add additional libraries to the generated
 // output files.
 //
@@ -19,5 +16,7 @@ app.import('vendor/captcha/captcha.js');
 // modules that you would like to import into your application
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
+
+app.import('vendor/captcha/captcha.js');
 
 module.exports = app.toTree();
