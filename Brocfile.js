d1bc7e6f89a6605f20d69ef05b6ba00eabaad560
Austin Trafficante <austin.trafficante@gmail.com>
2014-11-23 13:16:59 -0800

    Added support for Sass and imported assets.

--- Brocfile.js
+++ Brocfile.js
@@ -18,5 +18,6 @@ var app = new EmberApp();
 // along with the exports of each module as its value.
 
 app.import('vendor/captcha/captcha.js');
+app.import('bower_components/recaptcha-ajax/recaptcha_ajax.js');
 
 module.exports = app.toTree();
