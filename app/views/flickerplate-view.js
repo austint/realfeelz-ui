62ca12bba2aeda778d5ba6868c4e379b8aeb64c8
Leslie Strauss <leslie.strauss@gmail.com>
2015-01-08 05:28:34 -0800

    stuff.

--- app/views/flickerplate-view.js
+++ app/views/flickerplate-view.js
@@ -3,12 +3,12 @@ import Ember from 'ember';
 export default Ember.View.extend({
   didInsertElement: function() {
     Ember.$(document).ready(function() {
-      Ember.$('.slideshow.bg-image.flicker-theme-light.animate-transform-slide').flickerplate({
+      Ember.$('.slideshow.bg-image').flickerplate({
         auto_flick: true,
         flick_animation: 'transform-slide',
         auto_flick_delay: "5",
         flick_position: "1",
-        theme: "light",
+        theme: "dark",
         arrows: "false"
       });
     });
