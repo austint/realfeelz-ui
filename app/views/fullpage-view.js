1ce6c3748072913820e26c771b657c35f631e8b3
Leslie Strauss <leslie.strauss@gmail.com>
2015-01-07 23:47:17 -0800

    About to create a branch from test2-ui.

--- app/views/fullpage-view.js
+++ app/views/fullpage-view.js
@@ -4,10 +4,11 @@ export default Ember.View.extend({
   didInsertElement: function() {
     Ember.$(document).ready(function() {
         Ember.$('#fullpage').fullpage({
-          anchors: ['firstPage'],
-          sectionsColor: ['#4A6FB1'],
+          // anchors: ['firstPage', 'second'],
+          // sectionsColor: ['#4A6FB1'],
           autoScrolling: false,
-          css3: true
+          css3: true,
+          responsive: 900
         });
       });
   }
