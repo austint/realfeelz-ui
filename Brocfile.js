651841fe67b8823165ef865d74b4dfadbd128b52
Leslie Strauss <leslie.strauss@gmail.com>
2015-01-08 04:51:06 -0800

    Slideshow functionality working.

--- Brocfile.js
+++ Brocfile.js
@@ -28,7 +28,14 @@ app.import('bower_components/recaptcha-ajax/recaptcha_ajax.js');
 // app.import('bower_components/foundation/js/vendor/jquery.cookie.js');
 // app.import('bower_components/foundation/js/vendor/placeholder.js')
 
+app.import('bower_components/flickerplate/js/flickerplate.js');
+app.import('bower_components/flickerplate/js/jquery-v1.10.2.js');
+app.import('bower_components/flickerplate/js/modernizr-custom-v2.7.1.js');
+// hammer may cause issues
+// app.import('bower_components/flickerplate/js/hammer-v2.0.3.js');
+app.import('bower_components/flickerplate/css/flickerplate.css');
 
+app.import('bower_components/webplate/core/js/flickerplate.js');
 app.import('bower_components/webplate/core/js/min/webplate.min.js');
 app.import('bower_components/webplate/core/css/webplate.css');
 app.import('bower_components/webplate/project/icon-font/icomoon/style.css');
@@ -36,10 +43,10 @@ app.import('bower_components/webplate/project/ui/stripe/style.css');
 app.import('bower_components/webplate/project/ui/stripe/script.js');
 app.import('bower_components/webplate/stack.js');
 
+
 // app.import('bower_components/webplate/core/js/jquery.js');
 // app.import('bower_components/webplate/core/js/modernizr.js');
 // app.import('bower_components/webplate/core/js/hammer.js');
-// app.import('bower_components/webplate/core/js/flickerplate.js');
 
 
 // app.import('bower_components/hammerjs/hammer.js');
@@ -48,13 +55,6 @@ app.import('bower_components/webplate/stack.js');
 // app.import('bower_components/fullpage.js/jquery.fullPage.js');
 // fullpage css incompatible with fullbackground page scrolling functionality
 // app.import('bower_components/fullpage.js/jquery.fullPage.css');
-
-// app.import('bower_components/flickerplate/js/flickerplate.js');
-// app.import('bower_components/flickerplate/js/jquery-v1.10.2.js');
-// app.import('bower_components/flickerplate/js/modernizr-custom-v2.7.1.js');
-// hammer may cause issues
-// app.import('bower_components/flickerplate/js/hammer-v2.0.3.js');
-// app.import('bower_components/flickerplate/css/flickerplate.css');
 
 // app.import('bower_components/sticky-kit/jquery.sticky-kit.js');
 
