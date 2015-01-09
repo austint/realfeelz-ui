795487789bec0c99aab03543d5e43e7243e3d366
Leslie Strauss <leslie.strauss@gmail.com>
2015-01-09 01:51:58 -0800

    Added image files.

--- app/views/flickerplate-view.js
+++ app/views/flickerplate-view.js
@@ -4,9 +4,9 @@ export default Ember.View.extend({
   didInsertElement: function() {
     Ember.$(document).ready(function() {
       Ember.$('.flickerplate').flickerplate({
-        auto_flick: true,
+        auto_flick: false,
         flick_animation: 'transform-slide',
-        auto_flick_delay: "5"
+        // auto_flick_delay: "5"
       });
     });
   }
