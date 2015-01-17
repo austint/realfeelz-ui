c0a16c610a90830a7e265e18784f33380d4eeee0
Leslie Strauss <leslie.strauss@gmail.com>
2015-01-16 21:48:45 -0800

    Removed shrink part of message-input, now scroller doesn't appear when focus moves to button.

--- app/views/cleartext-view.js
+++ app/views/cleartext-view.js
@@ -6,14 +6,6 @@ export default Ember.View.extend({
       Ember.$('#clearButton').click(function() {
         Ember.$('#textbox').val('');
       });
-      // Ember.$(function() {
-      //   // Wire up your event (in this case we will handle this for all text inputs)
-      //   Ember.$("button[type='submit']").click(function() {
-      //     // When this is clicked, clear its value
-      //     Ember.$("textarea").val('');
-      //   });
-      // });
-
     });
   }
 });
