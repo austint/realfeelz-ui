8fa2648256eb0968ed01a51b43066d680d01960c
Leslie Strauss <leslie.strauss@gmail.com>
2015-01-16 16:03:23 -0800

    Added invisible textarea with display:none for honeypot, removed recaptcha, added style to loading page.

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,14 +1,14 @@
-import DS from 'ember-data';
-import ENV from '../config/environment';
-
-export default DS.RESTAdapter.extend({
-  namespace: 'api',
-  host: ENV.host
-});
-
-// import DS from "ember-data";
+// import DS from 'ember-data';
+// import ENV from '../config/environment';
 
 // export default DS.RESTAdapter.extend({
 //   namespace: 'api',
-//   host: 'https://realfeelz-server.herokuapp.com'
+//   host: ENV.host
 // });
+
+import DS from "ember-data";
+
+export default DS.RESTAdapter.extend({
+  namespace: 'api',
+  host: 'https://realfeelz-server.herokuapp.com'
+});
