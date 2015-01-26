f7b2702230d6a334746051a47afa2ba9c35a99fe
Leslie Strauss <leslie.strauss@gmail.com>
2015-01-25 19:30:24 -0800

    Added jquery mousewheel to csp script-src.

--- config/environment.js
+++ config/environment.js
@@ -19,7 +19,7 @@ module.exports = function(environment) {
     contentSecurityPolicy: {
       'report-uri': "http://localhost:4200",
       'default-src': "'none'",
-      'script-src': "'self' 'unsafe-eval' 'unsafe-inline",
+      'script-src': "'self' 'unsafe-eval' 'unsafe-inline' http://cdnjs.cloudflare.com/ajax/libs/jquery-mousewheel/3.1.12/jquery.mousewheel.min.js https://cdnjs.cloudflare.com/ajax/libs/jquery-mousewheel/3.1.12/jquery.mousewheel.min.js",
       'font-src': "'self' http://fonts.gstatic.com https://fonts.gstatic.com",
         'connect-src': "'self' https://realfeelz-server.herokuapp.com/api/statements http://realfeelz-server.herokuapp.com/api/statements https://realfeelz-server.herokuapp.com http://realfeelz-server.herokuapp.com",
       'img-src': "'self'",
