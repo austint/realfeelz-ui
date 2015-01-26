82174f101d8d3fe8b0fa22f3089fb81b96061b89
Leslie Strauss <leslie.strauss@gmail.com>
2015-01-25 20:10:55 -0800

    Working on preventing overscroll in Safari. Added custom view and css. Not going to be a piece of cake.

--- app/views/malihu-view.js
+++ app/views/malihu-view.js
@@ -3,11 +3,11 @@ import Ember from 'ember';
 export default Ember.View.extend({
   didInsertElement: function() {
     Ember.$(document).ready(function() {
-      Ember.$(window).load(function() {
+      // Ember.$(window).load(function() {
         Ember.$("#content-1").mCustomScrollbar({
           theme: "minimal"
         });
       });
-    });
+    // });
   }
 });
